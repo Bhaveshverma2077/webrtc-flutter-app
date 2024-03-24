4f14aef38fbd49a0bff5eae8ef763465cbc79c3e
Bhavesh Verma <bhaveshverma2077@gmail.com>
2024-03-24 22:12:53 +0530

    fix: onIceCandidate not triggering

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { Server } = require("socket.io");
 const express = require("express");
 const { createServer } = require("http");
 
-const rooms = {};
+const rooms = { some: { sockets: [] } };
 
 const app = express();
 const server = createServer(app);
@@ -11,6 +11,60 @@ const io = new Server(server);
 app.use(express.static("public"));
 
 io.on("connection", (socket) => {
+  socket.on("JOINROOM", (roomId) => {
+    console.log(`join ${roomId}`);
+    if (!rooms[roomId]) {
+      socket.emit("NOROOMWITHID");
+      return;
+    }
+    socket.on("icecandidate", (data) => {
+      const candidate = data.candidate;
+      const sdpMid = data.sdpMid;
+      const sdpMLineIndex = data.sdpMLineIndex;
+      const socketId = data.socketId;
+      io.to(socketId).emit("icecandidate", {
+        candidate,
+        sdpMid,
+        sdpMLineIndex,
+        socketId: socketId,
+      });
+    });
+    rooms[roomId].sockets.forEach((s) => {
+      console.log("1");
+      s.emit("SENDOFFER", { socketId: socket.id });
+      console.log("2");
+      s.on("icecandidate", (data) => {
+        const candidate = data.candidate;
+        const sdpMid = data.sdpMid;
+        const sdpMLineIndex = data.sdpMLineIndex;
+        socket.emit("icecandidate", {
+          candidate,
+          sdpMid,
+          sdpMLineIndex,
+          socketId: s.id,
+        });
+      });
+      s.on("SENDOFFER", (data) => {
+        console.log("3");
+        const offer = data.offer;
+        console.log(
+          "REC",
+          rooms[roomId].sockets.map((_, i) => `S${i}`)
+        );
+        socket.emit("RECIEVEOFFER", { offer, socketId: s.id });
+        socket.on("SENDANSWER", ({ answer }) => {
+          console.log("4");
+          s.emit("RECIEVEANSWER", { answer, socketId: socket.id });
+        });
+      });
+    });
+    rooms[roomId].sockets.push(socket);
+    socket.emit("JOINEDROOM");
+  });
+  socket.on("CREATEROOM", () => {
+    rooms[roomId] = { sockets: [] };
+    socket.emit("CREATEDROOMID", "ROOMID");
+  });
   console.log(`new socket connected ${socket.id}`);
   socket.on("offer", (data) => {
     const roomId = Math.random().toString();
